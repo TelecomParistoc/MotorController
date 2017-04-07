5ac4d8dab7b11919fb6034cb42610ae723a54750
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.org>
2017-04-07 21:40:38 +0200

    Improvement of the test

--- test/test_orientation.c
+++ test/test_orientation.c
@@ -7,14 +7,14 @@ extern int32_t test_orientation(void)
 {
     angular_trust_threshold = 0;
     wheels_gap = 5;
-    ticks_per_cm = 10;
+    ticks_per_cm = 50;
     orientation = 0;
 
     previous_left_ticks = 0;
     previous_right_ticks = 0;
 
     left_ticks = 0;
-    right_ticks = 79;
+    right_ticks = 392;
 
     compute_movement();
     update_orientation();
