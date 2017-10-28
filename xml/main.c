e51e2532b012374f855f8287b7fd1efd208327f6
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.org>
2017-10-28 13:24:14 +0200

    Creation of i2c_interface header

--- xml/main.c
+++ xml/main.c
@@ -10,14 +10,25 @@
 #define ADDRESS_MAX_SIZE 5
 #define TYPE_MAX_SIZE 20
 
+#define I2C_INTERFACE_HEADER "i2c_interface.h"
+#define I2C_INTERFACE_HEADER_HEADER "#ifndef I2C_INTERFACE_H\n#define I2C_INTERFACE_H\n#include \"ch.h\"\n#include \"hal.h\"\n\n"
+#define I2C_INTERFACE_HEADER_FOOTER "\n#endif /* I2C_INTERFACE_H */"
+#define CREATE_ITF_HEADER 1
+
+
 typedef struct IntfEntry_s {
     char name[NAME_MAX_SIZE];
-    char address[ADDRESS_MAX_SIZE];
     char type[TYPE_MAX_SIZE];
+    uint8_t address;
     uint8_t size;
     uint8_t read_access;
     uint8_t write_access;
 } IntfEntry_t;
+
+
+#if CREATE_ITF_HEADER
+FILE *itf_header_file;
+#endif
 
 static int isNumeric(char c) {
     if ((c >= '0') && (c <= '9')) {
@@ -47,6 +58,25 @@ static int get_size(char* type) {
     return size;
 }
 
+static int get_addr(char *addr_str) {
+    int index;
+    int multiplier = 1;
+    int addr = 0;
+
+    if (addr_str == NULL) {
+        return 255;
+    }
+
+    index = strlen(addr_str) - 1;
+    while(isNumeric(addr_str[index])) {
+        addr += (addr_str[index] - '0') * multiplier;
+        index--;
+        multiplier *= 16;
+    }
+
+    return addr;
+}
+
 static void parse_entry(xmlNode* node, IntfEntry_t* entry) {
     xmlNode *entry_element;
     entry_element = node->children;
@@ -57,7 +87,7 @@ static void parse_entry(xmlNode* node, IntfEntry_t* entry) {
             snprintf(entry->type, 20, "%s", entry_element->children->content);
             entry->size = get_size(entry->type);
         } else if (strcmp(entry_element->name, "address") == 0) {
-            snprintf(entry->address, 5, "%s", entry_element->children->content);
+            entry->address = get_addr(entry_element->children->content);
         } else if (strcmp(entry_element->name, "read") == 0) {
             if (strcmp(entry_element->children->content, "true") == 0)
                 entry->read_access = 1;
@@ -69,43 +99,84 @@ static void parse_entry(xmlNode* node, IntfEntry_t* entry) {
     }
 }
 
-static void print_entry(IntfEntry_t* entry, int fd) {
+static int is_small(char c) {
+    if ((c >= 'a') && (c <= 'z')) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+static void print_entry_interface_header(IntfEntry_t* entry) {
+    int index = 0;
+
+    fprintf(itf_header_file, "#define ");
+
+    while ((entry->name[index] != ' ') && (entry->name[index] != '\0')) {
+        if(is_small(entry->name[index])) {
+            fputc(entry->name[index] + 'A' - 'a', itf_header_file);
+        } else {
+            fputc(entry->name[index], itf_header_file);
+        }
+        index++;
+    }
+
+    if (entry->size == 32) {
+        fprintf(itf_header_file, "_LOW");
+    }
+
+    fprintf(itf_header_file, "_ADDR 0x%02x\n", entry->address);
+
+    if (entry->size == 32) {
+        fprintf(itf_header_file, "#define ");
+        index = 0;
+
+        while ((entry->name[index] != ' ') && (entry->name[index] != '\0')) {
+            if(is_small(entry->name[index])) {
+                fputc(entry->name[index] + 'A' - 'a', itf_header_file);
+            } else {
+                fputc(entry->name[index], itf_header_file);
+            }
+            index++;
+        }
+        fprintf(itf_header_file, "_HIGH_ADDR 0x%02x\n", entry->address + 2);
+    }
+}
+
+static void print_entry(IntfEntry_t* entry, FILE *file) {
     if (entry == NULL) {
         return;
     }
 
-    write(fd, "|", 1);
-    write(fd, entry->name, strlen(entry->name));
-    write(fd, "|", 1);
-    write(fd, entry->address, strlen(entry->address));
-    write(fd, "|", 1);
+    fprintf(file, "|%s|0x%02x|", entry->name, entry->address);
     if (entry->read_access) {
-        write(fd, "R", 1);
+        fputc('R', file);
     }
     if (entry->write_access) {
         if (entry->read_access)
-            write(fd, "/", 1);
-        write(fd, "W", 1);
+            fputc('/', file);
+        fputc('W', file);
     }
-    write(fd, "|", 1);
-    write(fd, entry->type, strlen(entry->type));
-    write(fd, "|\n", 2);
+    fprintf(file, "|%s|\n", entry->type);
 }
 
-static void process_entry(xmlNode *node, int fd) {
+static void process_entry(xmlNode *node, FILE *file) {
     IntfEntry_t entry;
     parse_entry(node, &entry);
-    print_entry(&entry, fd);
+    print_entry(&entry, file);
+#if CREATE_ITF_HEADER
+    print_entry_interface_header(&entry);
+#endif
 }
 
-static void print_element(xmlNode *node, int fd) {
+static void print_element(xmlNode *node, FILE *file) {
     xmlNode *cur_node;
     for (cur_node = node; cur_node; cur_node = cur_node->next) {
         if (cur_node->type == XML_ELEMENT_NODE) {
             if (strcmp(cur_node->name, "register") == 0) {
-                process_entry(cur_node, fd);
+                process_entry(cur_node, file);
             } else {
-                print_element(cur_node->children, fd);
+                print_element(cur_node->children, file);
             }
 
             /*if (cur_node->properties != NULL) {
@@ -125,17 +196,33 @@ int main(int argc, char* argv[]) {
     LIBXML_TEST_VERSION
     xmlDoc *doc;
     xmlNode *root_element;
-    int fd;
+    FILE *file;
 
     doc = xmlReadFile(argv[1], NULL, 0);
-    fd = creat(argv[2], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-    if (fd < 0) {
+    file = fopen(argv[2], "w");
+    if (file == NULL) {
         printf("Error when opening file %s\n", argv[2]);
         return 1;
     }
-    write(fd, "|Name|Address|Access|Size (in bits)|\n|----|-------|------|--------------|\n", 74);
+
+#if CREATE_ITF_HEADER
+    itf_header_file = fopen(I2C_INTERFACE_HEADER, "w");
+    if (itf_header_file == NULL) {
+        printf("Error when creating file %s\n", I2C_INTERFACE_HEADER);
+        return 1;
+    }
+    fprintf(itf_header_file, I2C_INTERFACE_HEADER_HEADER);
+#endif
+
+    fprintf(file, "|Name|Address|Access|Size (in bits)|\n|----|-------|------|--------------|\n");
     root_element = xmlDocGetRootElement(doc);
-    print_element(root_element, fd);
+    print_element(root_element, file);
+
+#if CREATE_ITF_HEADER
+    fprintf(itf_header_file, I2C_INTERFACE_HEADER_FOOTER);
+    fclose(itf_header_file);
+#endif
+    fclose(file);
 
     xmlFreeDoc(doc);
     xmlCleanupParser();
