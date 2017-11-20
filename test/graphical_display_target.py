bd443d55d0379c1f9d9c7c23db8583d3ddad92fb
ndeconto <nicodeconto@hotmail.fr>
2017-11-20 23:15:46 +0100

    clean workpace to unit test for compute target

--- test/graphical_display_target.py
+++ test/graphical_display_target.py
@@ -1,6 +1,20 @@
 from matplotlib.pyplot import *
+from subprocess import call, check_output
+from sys import argv
 
-with open("data_test/data.txt", "r") as f:
+temp_file = "data_test/targets.txt"
+print("#---------- CHECK TARGET COMPUTATION ---------- #")
+
+if len(argv) != 5:
+    print("Wrong number of parameters")
+    print("Usage : python this_file.py a_m a_d v_c x_f")
+    exit()
+
+call("make target_computation", shell=True)
+with open(temp_file, "w") as f:
+    call("./target_computation " + " ".join(argv[1:]), stdout=f, shell=True)
+
+with open(temp_file, "r") as f:
 
     #attention, il faut virer le \n final
     l_x = map(float, f.readline().split(',')[:-1])
--- test/test_target_computation.c
+++ test/test_target_computation.c
@@ -1,33 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <math.h>
+
 
 #define SIGN(x) (x > 0 ? 1 : (-1))
+#define ABS(x) (x > 0 ? x : -x)
 
 float get_position(float, float, float, float, float);
 
 /*
 prend en argument :
     le temps t
-    les caractéristiques de la courbe de vitesse, à savoir :
-        a_montante : accélération lors de la phase d'augmentation de vitesse
-        a_descendante : accélération lors de la phase de freinage
-        v_croisiere : vitesse de croisière
+    les caractï¿½ristiques de la courbe de vitesse, ï¿½ savoir :
+        a_montante : accï¿½lï¿½ration lors de la phase d'augmentation de vitesse
+        a_descendante : accï¿½lï¿½ration lors de la phase de freinage
+        v_croisiere : vitesse de croisiï¿½re
         x_final : la destination du robot
 
-    NB : a_montante et a_descendante doivent être de signe opposé
-    (a_montante peut être négative dans le cas où le robot recule)
+    NB : a_montante et a_descendante doivent ï¿½tre de signe opposï¿½
+    (a_montante peut ï¿½tre nï¿½gative dans le cas oï¿½ le robot recule)
 
-renvoie la position à l'instant t
+renvoie la position ï¿½ l'instant t
 
-pour memoire, la courbe de vitesse ressemble à :
+pour memoire, la courbe de vitesse ressemble ï¿½ :
 
          ---------------------
         /                     \
        /                       \
 ------/                         \--------
 
-(ou (-1) *  ça )
+(ou (-1) *  ï¿½a )
 
 car c'est la courbe qui permet de minimiser le temps pour atteindre x_final
 tout en ayant une vitesse continue (en accord avec la physique)
@@ -37,49 +40,43 @@ float get_position(float t, float a_montante, float a_descendante,
                    float v_croisiere, float x_final)
 {
 
+    if (x_final >= 0) {
+      a_montante = ABS(a_montante);
+      a_descendante = -ABS(a_descendante);
+      v_croisiere = ABS(v_croisiere);
+    }
+    else {
+      a_montante = - ABS(a_montante);
+      a_descendante = ABS(a_descendante);
+      v_croisiere = - ABS(v_croisiere);
+    }
+
 
     float t1 = v_croisiere / a_montante;
     float t2 = x_final / v_croisiere + v_croisiere / 2 * (1 / a_montante + 1 / a_descendante);
 
-    //printf("%.3f \t %.3f\n", t1, t2);
-
-
-    //cas où on atteint jamais la vitesse de croisière
+    //cas oï¿½ on atteint jamais la vitesse de croisiï¿½re
     if (t2 <= t1){
-
         float t4_carre = 2 * x_final / (a_montante * (1 - a_montante / a_descendante));
 
         if (t * t <= t4_carre) return a_montante * t * t / 2;
 
-        //calcul (demoniaque) de 2 / sqrt(t4_carre)
-        //noter qu'il s'agit quand même d'une valeur approchée...
-        //mais avec 3 décimales exactes
-        int i = *(int*)& t4_carre;
-        i = 0x5f3759df - (i >> 1);
-        float t4_inv = *(float*)&i;
-        t4_inv = t4_inv * (3.0f - (t4_carre * t4_inv * t4_inv));
-
+        float t4_inv = 2 / sqrt(t4_carre);
         float delta_t = t - t4_inv * x_final / a_montante;
 
         if (delta_t >= 0) return x_final;
-
         return a_descendante / 2 * delta_t * delta_t + x_final;
-
     }
 
     float t3 = t2 - v_croisiere /a_descendante;
-
 
     if (t < 0) return 0;
     else if (t <= t1 && t <= t2) return a_montante * t * t / 2;
     else if (t <= t2) return t1 * v_croisiere / 2 + v_croisiere * (t - t1);
     else if (t <= t3)
-        return x_final + v_croisiere * v_croisiere / (2 * SIGN(x_final) * a_descendante) + (t - t2) * (v_croisiere + SIGN(x_final) * a_descendante * (t - t2) / 2);
-        //return x_final + v_croisiere * v_croisiere / 2 / a_descendante \
-        //    + (t - t2) * (v_croisiere + a_descendante / 2 * (t - t2));
+      return x_final + v_croisiere * v_croisiere / 2 / a_descendante \
+              + (t - t2) * (v_croisiere + a_descendante / 2 * (t - t2));
     else return x_final;
-
-
 }
 
 int main(int argc, char* argv [])
@@ -99,19 +96,12 @@ int main(int argc, char* argv [])
     float v_c = strtof(argv[3], NULL);
     float x_f = strtof(argv[4], NULL);
 
-    /*for (i = 0; i < N; i++){
-
-        printf("x(%.2f) = %.4f\n", ((float) i) / N * t_max, get_position(((float) i) / N * t_max, 1., -2., 3, 15));
-    }*/
-
     for (i = 0; i < N; i++)printf("%f, ", ((float) i) / N * t_max);
     puts("");
     for (i = 0; i < N; i++){
-        //get_position(((float) i) / N * t_max, a_m, a_d, v_c, x_f);
         printf("%f, ", get_position(((float) i) / N * t_max, a_m, a_d, v_c, x_f));
     }
 
     puts("");
-
     return 0;
 }
