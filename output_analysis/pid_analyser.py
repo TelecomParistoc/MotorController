7360884a422d4f354bcd72465cd750267ade5930
ndeconto <nicodeconto@hotmail.fr>
2018-03-03 23:12:55 +0100

    WIP

--- output_analysis/pid_analyser.py
+++ output_analysis/pid_analyser.py
@@ -41,15 +41,24 @@ with open("output.txt", "r") as f:
     plt.plot(zip(*lin_pid)[0], label="lin_p")
     plt.plot(zip(*lin_pid)[1], label="lin_i")
     plt.plot(zip(*lin_pid)[2], label="lin_d")
-    """plt.plot(zip(ang_pid)[0], label="ang_p")
-    plt.plot(zip(ang_pid)[1], label="ang_i")
-    plt.plot(zip(ang_pid)[2], label="ang_d")"""
+    plt.plot(zip(*ang_pid)[0], label="ang_p")
+    plt.plot(zip(*ang_pid)[1], label="ang_i")
+    plt.plot(zip(*ang_pid)[2], label="ang_d")
     plt.title("PID weight repartition")
     plt.legend()
 
-    #to open a new window
     plt.figure()
+    plt.plot(pos, label="position")
+    plt.plot(target, label="target")
+    plt.legend()
+    plt.title("Position and target")
+
+    plt.figure()
+    plt.plot(lin_acc_saturation, label="lin_acc / max_lin_acc")
+    plt.plot(ang_acc_saturation, label="ang_acc / max_ang_acc")
+    plt.plot(l_sat, label="left saturation") # = command / MAX_PWM
+    plt.plot(r_sat, label="right saturation")
+    plt.legend()
+    plt.title("Saturation")
 
     plt.show()
-
-input()
